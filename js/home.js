eb87c338fa77a2fc3ad20990f001a439c0ba4f02
Eduardo87h7 <eduardogarciar160998>
2022-08-08 13:24:42 -0500

    Configuracion del Formulario

--- js/home.js
+++ js/home.js
@@ -7,13 +7,27 @@ $(document).ready(function(){
     });
   });
 
+  $(document).ready(function(){
+    $("#myInput2").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable2 tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+
+  document.getElementById("tabla2").style.display = "none";
+
 
 function tablas(){
     document.getElementById("tabla1").style.display = "none";
+    
+  document.getElementById("tabla2").style.display = "block";
+
 
 }
   
 function tablass(){
     document.getElementById("tabla1").style.display = "block";
-    
+    document.getElementById("tabla2").style.display = "none";
 }

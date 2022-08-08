1fb556e4f8f4796d87e950b51116ee87cca26ab1
Eduardo87h7 <eduardogarciar160998>
2022-08-08 14:51:30 -0500

    Configuracion de interfaz

--- js/home.js
+++ js/home.js
@@ -16,18 +16,34 @@ $(document).ready(function(){
     });
   });
 
+  $(document).ready(function(){
+    $("#myInput3").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable3 tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+  
+
+
+
   document.getElementById("tabla2").style.display = "none";
+  document.getElementById("tabla3").style.display = "none";
 
 
 function tablas(){
     document.getElementById("tabla1").style.display = "none";
-    
   document.getElementById("tabla2").style.display = "block";
-
+  document.getElementById("tabla3").style.display = "none";
+  document.getElementById("trabb").innerHTML = "Todos Los Documentos";
 
 }
   
-function tablass(){
-    document.getElementById("tabla1").style.display = "block";
-    document.getElementById("tabla2").style.display = "none";
+
+function tablasss(){
+  document.getElementById("tabla3").style.display = "block";
+  document.getElementById("tabla1").style.display = "none";
+  document.getElementById("tabla2").style.display = "none";
+  document.getElementById("trabb").innerHTML = "Mis Documentos";
 }

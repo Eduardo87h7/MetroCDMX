f2a33336ac94295193b62431267d5fa4e3cf5532
Eduardo87h7 <eduardogarciar160998>
2022-08-12 13:58:01 -0500

    configuracionm del directorio

--- carga.php
+++ carga.php
@@ -1,19 +1,19 @@
 <?php
-if(!file_exists("archivos")){
-    if(!mkdir("C:/Users/Eduardo/OneDrive - INSTITUTO TECNOLOGICO DE GUSTAVO A MADERO/Escritorio/documentos/",0777)){
-        echo "error del directorio";
-        exit();
 
+
+
+if(!file_exists("archivos")){
+    if(!mkdir("archivos",0777)){
+        echo "Error al crear el directorio";
+        exit();
     }
 }
 
+
 chmod("archivos",0777);
 
-if(move_uploaded_file($_FILES['fichero']['tmp_name'],"C:/Users/Eduardo/OneDrive - INSTITUTO TECNOLOGICO DE GUSTAVO A MADERO/Escritorio/documentos/".$_FILES['fichero']['name'])){
-    echo "archivi subido";
-}else {
-    echo "erro ";
-    }
-
-
-?>
+if(move_uploaded_file($_FILES['fichero']['tmp_name'],"C:/Users/Eduardo/OneDrive - INSTITUTO TECNOLOGICO DE GUSTAVO A MADERO/Escritorio/archivos/".$_FILES['fichero']['name'])){
+    echo "Archivo subido con exito";
+}else{
+    echo "Error al subir el archivo";
+}
--- layout/carga.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo $_FILES['fichero']['name']. "<br>"
-
-?>

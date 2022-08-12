4fb89f2dea8b3b605f11aa15c10a63e15af184d6
Eduardo87h7 <eduardogarciar160998>
2022-08-12 10:55:58 -0500

    arreglo para administador

---
+++ layout/registrar.php
@@ -0,0 +1,30 @@
+<?php 
+
+include("con_db.php");
+
+if (isset($_POST['register'])) {
+    if (strlen($_POST['name']) >= 1 && strlen($_POST['contraseña']) >= 1) {
+	    $usuario = trim($_POST['name']);
+	    $contraseña = trim($_POST['contraseña']);
+	    $tipo = trim($_POST['tipo']);
+	    $consulta = "INSERT INTO usuarios(usuario, contraseña, tipo ) VALUES ('$usuario','$contraseña','$tipo')";
+	    $resultado = mysqli_query($conex,$consulta);
+	    if ($resultado) {
+	    	?> 
+	    	<h3 >correxto</h3>
+           <?php
+	    } else {
+	    	?> 
+	    	<h3 >Ocurrio un error</h3>
+           <?php
+	    }
+    }   else {
+	    	?> 
+	    	<h3 >no hay campos</h3>
+           <?php
+    }
+}
+
+
+?>
+
